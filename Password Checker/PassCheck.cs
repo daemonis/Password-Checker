e10ba798bd06d0f0b691a58759ba551da88a1c9d
Melanie Zeller <melanie.zeller@verizon.net>
2022-02-08 21:46:51 -0800

    + Username restrictions

--- Password Checker/PassCheck.cs
+++ Password Checker/PassCheck.cs
@@ -81,6 +81,18 @@
             }
         }
 
+        static bool IsUsernameLong()
+        {
+            if (username.Length > 20 || username.Length < 8)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
         static void GetUsername()
         {
             username = Console.ReadLine();
@@ -97,14 +109,14 @@
                 {
                     Console.Write("You must enter a username.\nPlease enter a username: ");
                 }
+                else if (!IsUsernameLong())
+                {
+                    Console.Write("Your username must contain the following:\n- Between 8 and 20 characters.\nPlease try again: ");
+                }
             }
-            while (!IsUsernameThere());
+            while (!IsUsernameThere() || !IsUsernameLong());
 
             Console.Write($"Please set a password for user {username}: ");
-
-            bool passwordIsLong = false;
-            bool passwordIsCaps = false;
-            bool passwordHasNum = false;
 
             do
             {
@@ -115,17 +127,12 @@
                     Console.Write("You must enter a password.\nPlease enter a password: ");
                     continue;
                 }
-
-                passwordIsLong = IsPasswordLong();
-                passwordIsCaps = IsPasswordCaps();
-                passwordHasNum = IsPasswordNum();
-
-                if (!passwordIsLong || !passwordIsCaps || !passwordHasNum)
+                else if (!IsPasswordLong() || !IsPasswordCaps() || !IsPasswordNum())
                 {
                     Console.Write($"Your password must contain the following:\n- More than 8 characters.\n- A number.\n- A capital letter.\nPlease try again: ");
                 }
             }
-            while (!passwordIsLong || !passwordIsCaps || !passwordHasNum);
+            while (!IsPasswordLong() || !IsPasswordCaps() || !IsPasswordNum());
 
             Console.WriteLine("Your password meets our system criteria, welcome!.");
         }
